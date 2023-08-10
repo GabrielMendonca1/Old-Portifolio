36564fd6faf6962b16770d04d607af7bd42a6bdf
GabrielMendonca1 <garimeipod@gmail.com>
2023-08-10 14:36:30 -0300

    v1.0.3

--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,39 @@
+import { useState, useEffect } from 'react';
 import { Footer, Navbar } from '../components';
-import {
-  About,
-  Explore,
-  Feedback,
-  GetStarted,
-  Hero,
-  WhatsNew,
-} from '../sections';
+import { About, Explore, Feedback, GetStarted, Hero, WhatsNew } from '../sections';
 
-const Page = () => (
-  <div className="bg-primary-black overflow-hidden overflow-x-hidden ">
-    <Navbar />
-    <Hero />
-    <div className="relative">
-      <About />
-      <div className="gradient-03 z-0" />
-      <Explore />
+const Page = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(false);
+  }, []);
+
+  return (
+    <div className="bg-primary-black overflow-hidden overflow-x-hidden">
+      {loading ? (
+        <div className="fixed top-0 left-0 z-50 w-full h-full bg-primary-black flex items-center justify-center">
+          <div className="loader"></div>
+        </div>
+      ) : null}
+      <Navbar />
+      <Hero />
+      <div className="relative">
+        <About />
+        <div className="gradient-03 z-0" />
+        <Explore />
+      </div>
+      <div className="relative">
+        <GetStarted />
+        <div className="gradient-03 z-0" />
+        <WhatsNew />
+      </div>
+      <div>
+        <Feedback />
+      </div>
+      <Footer />
     </div>
-    <div className="relative">
-      <GetStarted />
-      <div className="gradient-03 z-0" />
-      <WhatsNew />
-    </div>
-    <div>
-      <Feedback />
-    </div>
-    <Footer />
-  </div>
-);
+  );
+};
 
 export default Page;
